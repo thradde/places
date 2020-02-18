4398876a325c2c3882638633b5e1abd818943950
Thorsten <support@idealsoftware.com>
2020-02-18 12:12:53 +0100

    added "registered to ralph jansen"

--- Places/main.cpp
+++ Places/main.cpp
@@ -1414,7 +1414,8 @@ public:
 			m_bChangeWindowLayout(false)
 	{
 #if 0
-		m_strLicense = _T("Registered to: Heinz Hartstein • Heinz-Hartstein@Arcor.de • Erlenweg  55 • 47877 Willich - Neersen");
+		//m_strLicense = _T("Registered to: Heinz Hartstein • Heinz-Hartstein@Arcor.de • Erlenweg  55 • 47877 Willich - Neersen • Deutschland");
+		m_strLicense = _T("Registered to: Ralph Jansen • ralph.jansen@gmx.ch • Gerberweg 57 • 2560 Nidau • Schweiz");
 #endif
 
 		gbHotkeyVKey = 0;		// virtual key code for app activation
