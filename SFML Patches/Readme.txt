9117b23b61a80c45535b99e38fdfc49cad091221
Thorsten <support@idealsoftware.com>
2020-10-29 14:13:27 +0100

    Fixed random crashes in DINPUT.DLL, which is related to joystick input. This happened after Places was running for several days or weeks. Therefore I disabled the SFML joystick processing, as it is not used in Places.

--- SFML Patches/Readme.txt
+++ SFML Patches/Readme.txt
@@ -1,3 +1,6 @@
 Places is compiled and linked against SFML v2.3.
 SFML was modified to support Drag & Drop.
+I also experienced random crashes in DINPUT.DLL, which is related to joystick input.
+This happened after Places was running for several days or weeks.
+Therefore I disabled the SFML joystick processing, as it is not used in Places.
 The patch files in this directory must be applied to the SFML 2.3 source code.
---
+++ SFML Patches/WindowImpl.cpp
@@ -0,0 +1,9 @@
+diff -u E:/source/SFML source 2.3 original/src/SFML/Window/WindowImpl.cpp E:/source/SFML source/src/SFML/Window/WindowImpl.cpp
+--- E:/source/SFML source 2.3 original/src/SFML/Window/WindowImpl.cpp	Sun May  3 21:53:53 2015
++++ E:/source/SFML source/src/SFML/Window/WindowImpl.cpp	Mon Sep 21 13:03:04 2020
+@@ -118 +118 @@
+-        processJoystickEvents();
++        //processJoystickEvents();
+@@ -131 +131 @@
+-                processJoystickEvents();
++                //processJoystickEvents();
